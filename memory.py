c9702de2c92cca58e8bc9457bc9e875a61bef300
Bruno Varandas <varandas.bv@gmail.com>
2026-05-23 16:36:13 +0100

    feat: added memory class

---
+++ memory.py
@@ -0,0 +1,62 @@
+import anthropic
+
+class MessageMemory():
+    
+    def __init__(self, max_buffer: int = 20):
+        
+        self.buffer: list[dict] = []
+        self.max_buffer: int = max_buffer
+        self.summary: str = ""
+        self.client: anthropic.Client = anthropic.Client()
+        
+        
+    def add_messages(self, role: str, message: str):
+        
+        self.buffer.append({
+            "role":role,
+            "content":message
+        })
+        
+    
+    def show_messages(self):
+        
+        return self.buffer
+    
+    
+    def summarize_and_reset_conversation(self):
+        
+        prompt = '''
+        Summarize the conversation given to initiate a new context window.
+        This needs to include all relevant information for the LLM to know what was previously discussed
+        '''
+
+        try:
+            # Create new temporary buffer to not contaminate original one
+            temp_buffer = self.buffer.copy()
+            
+            temp_buffer.append({
+                "role":"user",
+                "content":prompt
+            })
+            
+            # Calls LLM to summarize the entire conversation history
+            response = self.client.messages.create(
+                model="claude-haiku-4-5",
+                messages=temp_buffer
+            )
+            
+            summary = response.content[0].text
+            
+            # Save summary
+            self.summary = summary
+            
+            # Reset
+            self.buffer = []
+        
+        except Exception as e:
+            print("Error ocurred: ", e)
+            
+        
+        
+        
+        
